a25ba9b25870fe94d35868d8ac0d08ec6fa51c63
sveta-terentyeva <svetik.terentyeva@gmail.com>
2026-02-17 19:37:35 +0200

    Rounds_to_agreement uses ACCEPT in workflow mode

--- src/llmgt/sim/rounds.py
+++ src/llmgt/sim/rounds.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
+
 from typing import List, Optional
 
 from llmgt.logging.records import ChatMessage
 from llmgt.games.base import Game
 from llmgt.sim.agreement import agreement_hit
+from llmgt.sim.workflow import workflow_has_agreement
 
 
 def compute_rounds_to_agreement(
@@ -21,14 +23,18 @@ def compute_rounds_to_agreement(
     for r in range(1, max_comm_rounds + 1):
         upto = non_system[: 2 * r]
 
-        if agreement_hit(
-            game=game,
-            mode=mode,
-            messages=upto,
-            final_action_a=final_action_a,
-            final_action_b=final_action_b,
-        ):
-            return r
+        if mode == "workflow":
+            if workflow_has_agreement(upto):
+                return r
+        else:
+            if agreement_hit(
+                game=game,
+                mode=mode,
+                messages=upto,
+                final_action_a=final_action_a,
+                final_action_b=final_action_b,
+            ):
+                return r
 
     return None
 
